7e80ce36e29d9a57f6d1f644061db250b8aa59c2
Phil Lewis <phillewis1985@gmail.com>
2022-03-02 06:11:50 -0700

    Build basic Navbar, install sass and reset file

---
+++ src/Components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../Styles/Navbar.scss'
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>Jazz Collaborations</h1>
+
+    </nav>
+  )
+}
+
+export default Navbar
