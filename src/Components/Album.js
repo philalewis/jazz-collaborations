8fa142e46eb010938e5f3617fef821b479bc3f65
Phil Lewis <phillewis1985@gmail.com>
2022-03-03 09:59:00 -0700

    Style album page

--- src/Components/Album.js
+++ src/Components/Album.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useContext } from 'react'
 import { useLocation, useParams } from 'react-router-dom'
 import { Errors } from '../Contexts/ErrorContextProvider'
 import { getAlbum } from '../apiCalls'
+import '../Styles/Album.scss'
 
 const Album = () => {
   const [ album, setAlbum ] = useState(null)
@@ -10,10 +11,8 @@ const Album = () => {
 
   useEffect(() => {
     const id = location.pathname.split('/')[2]
-    console.log(id)
     getAlbum(id)
     .then(data => {
-      console.log(data)
       setAlbum(data)
     })
     .catch(error => setErrorMessage(error))
@@ -23,13 +22,25 @@ const Album = () => {
     return album !== null ?
       <>
         <img className="album-cover" src={album.cover} alt="album cover"/>
-        <h2 className="album-title">{}</h2>
-        <p className="release-year">Released: {}</p>
+        <h2 className="album-title">{album.title}</h2>
+        <h3 className="album-artist">{album.albumArtist}</h3>
+        <p className="release-year">Released: {album.releaseYear}</p>
+        { album.musicians && <section className="musicians-container">
+          {musicians()}
+        </section> }
       </> :
       <section>
         <p>Sorry, we could not find the album you are looking for.</p>
       </section>
   }
+
+  const musicians = () => album.musicians.map(musician => {
+    return (
+      <article key={musician.name} className="musician-info">
+        <p>{musician.name} ({musician.instrument})</p>
+      </article>
+    )
+  })
 
   return (
     <section className="album-page">
