d54bdeed31f20b982a44224d2117b05610a76e82
Phil Lewis <phillewis1985@gmail.com>
2022-03-05 21:53:27 -0700

    Test home page, paths, and error modal

--- cypress/integration/Home_spec.js
+++ cypress/integration/Home_spec.js
@@ -7,6 +7,7 @@ describe('Home page and search results', () => {
       .get('input:text')
       .type('John Coltrane')
       .get('.search-button').click()
+      .url().should('eq', 'http://localhost:3001/search?John%20Coltrane')
       .get('.add-search-results')
       .get('.add-results-button')
       .should('not.be.disabled')
@@ -24,8 +25,19 @@ describe('Home page and search results', () => {
       .get('.instructions')
       .get('.artist-link')
       .contains('John Coltrane').click()
-      
+      .url().should('eq', 'http://localhost:3001/artist/2')
   })
 
-  it('should ')
+  it('should display an error modal if there is a problem with a network request', () => {
+    cy.intercept('Get', 'http://localhost:3000/api/v1/musicians', {
+      statusCode: 500
+    })
+
+    cy.visit('http://localhost:3001/')
+      .get('.error-box')
+      .contains('Sorry, something went wrong. Please try again later.')
+      .get('.exit-error-modal-button').click()
+      .get('.error-box')
+      .should('not.exist')
+  })
 })
